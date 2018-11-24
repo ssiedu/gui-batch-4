861f48cbd2b7969755d63ad72ce830e88b8138ca
CLASSROOM01 <CLASSROOM01@CLASSROOM01-PC>
2018-11-24 17:32:31 +0530

    GUI-24-11-18

--- src/gui4/GuiFive.java
+++ src/gui4/GuiFive.java
@@ -1,19 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui4;
-
-/**
- *
- * @author CLASSROOM01
- */
 public class GuiFive extends java.awt.Frame {
-
-    /**
-     * Creates new form GuiFive
-     */
     public GuiFive() {
         initComponents();
         setSize(500,500);
